8d3e37ec59e4e8be65f460015054e1db7bda4807
Brodie <brodiechampion@gmail.com>
2020-06-17 01:24:09 -0300

    Tidy up! Prettier highlighting and overlays
    I gave up on using box sizing, since it broke
    the centering of the overlay circle. Not sure how to fix that.
    Instead, switched to black backgound border and a blue highlight.
    It definitely pops better now.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let imgHome = "https://beethacker.github.io/slidepuzzle/img/";
+
+
+function makeTranslateStyle(x, y) {
+    return {transform: `translate(${x}px, ${y}px)`};
+}
+
+function CenterCircle(props) {
+    if (!props.inCell) {
+        return <></>;
+    }
+    let className = "center-circle"; 
+    if (props.active) {
+        className += " active";
+    } 
+    return <div className={className} style={makeTranslateStyle(props.x, props.y)}></div>
+}
+
+function CroppedImage(props) {
+    return <img src={imgHome + "camping.jpg"} alt="stub" style={{
+                "marginTop" : -props.sy + "px",
+                "width": props.w + "px",
+                "height" : props.h + "px",
+                "marginLeft": -props.sx + "px",
+            }}/>
+}
+
 function Square(props) {
     const index = props.value;
+    const active = false;
+    const inCell = index === 3;
     const x = index % props.rows;
     const y = Math.floor(index / props.rows);
     let classes = "square";
-    if (index === 3) {
+    if (inCell) {
         classes += " selected";
+    }
+    let overlay = null;
+    if (inCell) {
+        overlay = <>
+        <div className="circle" />
+        <CenterCircle x={0.5*props.w} y={0.5*props.h} active={active} inCell={inCell}/>
+        </>
     }
     return (
         <td className={classes} onClick={() => props.handleClick()} style={{"width": props.w, "height": props.h}}>
-            <img src="img/camping.jpg" alt="stub" style={{
-                "margin-top" : -y*props.h + "px",
-                "width": props.cols*props.w + "px",
-                "height" : props.rows*props.h + "px",
-                "margin-left": -x*props.w + "px",
-            }}/>
+            {overlay}        
+            <CroppedImage w={props.cols*props.w} h={props.rows*props.h} sx={x*props.w} sy={y*props.h} inCell={inCell}/>
         </td>
     );
 }
@@ -130,6 +162,7 @@ class Game extends React.Component {
     render() {
         return (
             <div>
+                <div className="circle"/>
                 { this.state.hasLocation 
                 ? <p> Location: {this.state.coords.latitude}, {this.state.coords.longitude} </p> 
                 : <p> Location unavailable! </p> }    
